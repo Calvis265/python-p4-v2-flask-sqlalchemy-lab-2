e799a1eac977f2443e8619ab5b205f029d45885d
Calvis Onyango <calvisonyango265@gmail.com>
2025-06-25 13:55:50 +0300

    flaskalchemy

--- server/app.py
+++ server/app.py
@@ -7,15 +7,12 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-migrate = Migrate(app, db)
-
-db.init_app(app)
-
+db.init_app(app)      # Initialize db first
+migrate = Migrate(app, db)  # Then create migrate instance
 
 @app.route('/')
 def index():
     return '<h1>Flask SQLAlchemy Lab 2</h1>'
-
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
